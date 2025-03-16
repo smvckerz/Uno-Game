a820e13993f7066638c8b1e421b07e67a1d32460
eduardo <edmunoz1030@gmail.com>
2025-03-16 11:11:27 -0700

    Fixed more errors and adding final touches, unless more needs to be fixed

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,6 +1,9 @@
 import express from "express";
+
 const router = express.Router();
+
 router.get("/", (_request, response) => {
 response.render("root", { title: "Zeta Chi 1 site" });
 });
+
 export default router;
