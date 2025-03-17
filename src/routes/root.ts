df9d83ad35ec3b4fd1a55f85b21104cd5ce1848d
smvckerz <104466989+smvckerz@users.noreply.github.com>
2025-03-16 19:36:06 -0700

    fixed error, two routes directories, got rid of one

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (_request, response) => {
-response.render("root", { title: "Zeta Chi 1 site" });
+response.render("root", { name: "Zeta Chi 1 site" });
 });
 
 export default router;
